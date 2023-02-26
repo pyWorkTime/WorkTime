13b60f10ea916e06ee748bbe0a01737dd4dad120
WorkTime <WorkTime@com.com>
2023-02-26 11:45:04 -0800

    initial commit

---
+++ worktime.py
@@ -0,0 +1,32 @@
+import yaml
+from yaml.loader import SafeLoader
+from pathlib import Path
+import sys
+
+#  from this project
+from ui import menu
+
+
+def main():
+    ''' read in the user supplied yaml data then call the ui object '''
+
+
+    try:
+        filename = sys.argv[1]
+    except:
+        filename = 'default.yml'
+
+    print(filename)
+
+    p = Path(__file__).with_name(filename)
+    
+    with p.open('r') as f:
+        data = yaml.load(f, Loader=SafeLoader)
+
+    ui = menu(data)
+
+
+if __name__ == "__main__":
+    
+
+    main()
